89ac2f8bb84d5fa592d5ce9d35daa8a123217e8e
George Keratzakis <geokeratz@gmail.com>
2023-09-25 23:58:33 +0300

    Renamed and removed files, split script in cells and added audio and midi files

--- 20230925_MidiNote_Clip_Selector_b copy/20230113_MidiNote_Clip_Selector.py
+++ 20230925_MidiNote_Clip_Selector_b copy/20230113_MidiNote_Clip_Selector.py
@@ -1,3 +1,4 @@
+# %%
 import datetime
 # import math
 import random
@@ -7,37 +8,45 @@ from csv import reader
 # import py_midicsv
 from moviepy.editor import concatenate_videoclips, VideoFileClip, AudioFileClip, CompositeAudioClip, ImageClip
 
+# %%
 # mid = mido.MidiFile('20221213_Droner_Rander_[Clip_Selector]_test_03.mid', )
-input_audio = AudioFileClip('./20230109_Droner_Rander 2023-01-09 2109.mp3')
+input_audio = AudioFileClip('/media/user7/Double_Dragon/Videography/My_Python_Video_Projects/RGB/20230925_MidiNote_Clip_Selector_b copy/Bus_Stop_0013.mp3')
 
 
 # csv_string = py_midicsv.midi_to_csv("20221213_Droner_Rander_[Clip_Selector]_test_03.mid",)
-
 # with open("Droner_Rander_[Clip_Selector]_test_03.csv", "w") as f:
 #     f.writelines(csv_string)
 
 def select_clip_with_notes(note :int , dir :str) -> str:
     _note  :int = note
     _dir :str = dir
-    if _note == 60:
-        return f'{_dir}001.mp4'
-    elif _note == 61:
-        return f'{_dir}002.mp4'
-    elif _note == 62:
-        return f'{_dir}003.mp4'
-    elif _note == 63:
-        return f'{_dir}004.mp4'
-    elif _note == 64:
-        return f'{_dir}005.mp4'
-    elif _note == 65:
-        return f'{_dir}006.mp4'
-    elif _note == 66:
-        return f'{_dir}007.mp4'
-    elif _note == 67:
-        return f'{_dir}008.mp4'
-    else:
-        return (f'Note:{_note} is out of range')
+    assert _note >= 60 and _note <= 83, f'Note:{_note} is out of range'
+    if _note == 60: return f'{_dir}001.mp4'
+    if _note == 61: return f'{_dir}002.mp4'
+    if _note == 62: return f'{_dir}003.mp4'
+    if _note == 63: return f'{_dir}004.mp4'
+    if _note == 64: return f'{_dir}005.mp4'
+    if _note == 65: return f'{_dir}006.mp4'
+    if _note == 66: return f'{_dir}007.mp4'
+    if _note == 67: return f'{_dir}008.mp4'
+    if _note == 68: return f'{_dir}009.mp4'
+    if _note == 69: return f'{_dir}010.mp4'
+    if _note == 70: return f'{_dir}011.mp4'
+    if _note == 71: return f'{_dir}012.mp4'
+    if _note == 72: return f'{_dir}013.mp4'
+    if _note == 73: return f'{_dir}014.mp4'
+    if _note == 74: return f'{_dir}015.mp4'
+    if _note == 75: return f'{_dir}016.mp4'
+    if _note == 76: return f'{_dir}017.mp4'
+    if _note == 77: return f'{_dir}018.mp4'
+    if _note == 78: return f'{_dir}019.mp4'
+    if _note == 79: return f'{_dir}020.mp4'
+    if _note == 80: return f'{_dir}021.mp4'
+    if _note == 81: return f'{_dir}022.mp4'
+    if _note == 82: return f'{_dir}023.mp4'
+    if _note == 83: return f'{_dir}024.mp4'
     
+# %%
 
 # Initialize list to store video clips
 subclips = []
