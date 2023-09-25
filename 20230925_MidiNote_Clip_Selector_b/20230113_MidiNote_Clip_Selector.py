66057c2acda8f90d4340514d9f9be8d2efca3bc4
George Keratzakis <geokeratz@gmail.com>
2023-09-26 02:06:12 +0300

    Commited but IT CRASHES. Maybe from Memory?

--- 20230925_MidiNote_Clip_Selector_b/20230113_MidiNote_Clip_Selector.py
+++ 20230925_MidiNote_Clip_Selector_b/20230113_MidiNote_Clip_Selector.py
@@ -3,80 +3,54 @@ import datetime
 # import math
 import random
 from csv import reader
-
+import easygui
 # import mido
 import py_midicsv
 from moviepy.editor import concatenate_videoclips, VideoFileClip, AudioFileClip, CompositeAudioClip
 
 # %%
-input_audio = AudioFileClip('20230925_MidiNote_Clip_Selector_b/input_audio.mp3')
+input_audio_path = easygui.fileopenbox('./', filetypes=['*.mp3'], multiple=False, title='Select Audio File')
+input_audio = AudioFileClip(input_audio_path)
 
-csv_string = py_midicsv.midi_to_csv('./20230925_MidiNote_Clip_Selector_b/input_midi.mid', encoding='utf-8')
-# with open("Droner_Rander_[Clip_Selector]_test_03.csv", "w") as f:
-#     f.writelines(csv_string)
+midi_file_path = easygui.fileopenbox('./', filetypes=['*.mid'], multiple=False, title='Select Midi File')
+csv_string = py_midicsv.midi_to_csv(midi_file_path)
 
-def select_clip_with_notes(note :int , dir :str) -> str:
-    _note  :int = note
-    _dir :str = dir
-    assert _note >= 60 and _note <= 83, f'Note:{_note} is out of range'
-    if _note == 60: return f'{_dir}001.mp4'
-    if _note == 61: return f'{_dir}002.mp4'
-    if _note == 62: return f'{_dir}003.mp4'
-    if _note == 63: return f'{_dir}004.mp4'
-    if _note == 64: return f'{_dir}005.mp4'
-    if _note == 65: return f'{_dir}006.mp4'
-    if _note == 66: return f'{_dir}007.mp4'
-    if _note == 67: return f'{_dir}008.mp4'
-    if _note == 68: return f'{_dir}009.mp4'
-    if _note == 69: return f'{_dir}010.mp4'
-    if _note == 70: return f'{_dir}011.mp4'
-    if _note == 71: return f'{_dir}012.mp4'
-    if _note == 72: return f'{_dir}013.mp4'
-    if _note == 73: return f'{_dir}014.mp4'
-    if _note == 74: return f'{_dir}015.mp4'
-    if _note == 75: return f'{_dir}016.mp4'
-    if _note == 76: return f'{_dir}017.mp4'
-    if _note == 77: return f'{_dir}018.mp4'
-    if _note == 78: return f'{_dir}019.mp4'
-    if _note == 79: return f'{_dir}020.mp4'
-    if _note == 80: return f'{_dir}021.mp4'
-    if _note == 81: return f'{_dir}022.mp4'
-    if _note == 82: return f'{_dir}023.mp4'
-    if _note == 83: return f'{_dir}024.mp4'
+with open('auto_saved.csv', 'w', encoding='UTF8') as f:
+    csv_with_no_header = csv_string[5:]
+    f.writelines(csv_with_no_header)
+    
+
+# %%
+def select_clip_with_notes(note: int, dir: str) -> str:
+    assert 36 <= note <= 63, f'Note: {note} is out of range'
+    video_number = note - 35  # Calculate video number
+    print(f'note: {note}')
+    print(f'video_number: {video_number}')
+    return f'{dir}{video_number:03d}.MP4'
+
     
 # %%
-
 # Initialize list to store video clips
 subclips = []
-
 # Initialize starting time for video clips
 sub_startime = 0
 concat_vid_duration = 0
 concat_start_time = 0
 
-
-with open("Droner_Rander_[Clip_Selector]_test_03.csv", "r") as f:
+# %%
+with open('auto_saved.csv', 'r', encoding='UTF8') as f:
     csv_reader = reader(f, delimiter=',')
     for row in csv_reader:
-        if row[2] == ' Note_off_c':
-            
+        if row[0]=='4' and row[2] == ' Note_off_c':
+            print(f'{row}')
             note_off_time = float(row[1]) /1920  
             duration = note_off_time - concat_vid_duration
-            clip = VideoFileClip(select_clip_with_notes(int(row[4]), './INPUT_VIDEOS/'), audio=False)
+            clip = VideoFileClip(select_clip_with_notes(int(row[4]), '/media/user7/Double_Dragon/Videography/My_Python_Video_Projects/RGB/INPUT_VIDEOS/'), audio=False)
             sub_startime = random.random()*(clip.duration - 1)
             sub = clip.subclip(
                 sub_startime,
                 sub_startime + duration
                 )
-            
-            # random_frame = clip.get_frame(
-            #     random.randint(
-            #         0, 10*int(clip.duration)
-            #         )*10
-            #                             )
-            # clip = ImageClip(random_frame).set_duration(note_off_time-concat_vid_duration)
-            
-            # if start_time + clip_duration > 0
             # Append the subclip to the list of video clips
             subclips.append(sub)
 
@@ -88,4 +62,5 @@ final_video = concatenate_videoclips(subclips).set_fps(60)
 
 now = datetime.datetime.now()
 final_video.audio = CompositeAudioClip([input_audio])
-final_video.write_videofile(f'./{now}.mp4',)
+final_video.write_videofile(f'./{now}.MP4',)
+# %%
